8d03a5c7c6830b78abfa1d03476b7bbfaad0e21a
aryan114 <amberkararyan@gmail.com>
2020-12-17 21:32:59 -0800

    Initial Commit

--- src/Sports/Sports.java
+++ src/Sports/Sports.java
@@ -1,5 +1,54 @@
 package Sports;
 // Made by Aryan Amberkar
-public class Sports {
+
+import javax.swing.*;
+import javax.swing.border.MatteBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Sports extends JFrame {
+    private JLabel team1 = new JLabel();
+    private JLabel team2 = new JLabel();
+    private JButton calculate = new JButton();
+    private JTextField team1entry = new JTextField();
+    private JTextField team2entry = new JTextField();
+
+    public Sports() {
+        getContentPane().setBackground(Color.CYAN);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setBounds(300,300, 1300,400);
+        getContentPane().setLayout(null);
+
+        team1.setForeground(Color.white);
+        team2.setForeground(Color.white);
+        team1.setHorizontalAlignment(SwingConstants.CENTER);
+        team2.setHorizontalAlignment(SwingConstants.CENTER);
+        team1.setText("Enter your first team: ");
+        team2.setText("Enter your second team: ");
+        team1.setBounds(5,10,500,100);
+        team2.setBounds(5,105,500,100);
+        getContentPane().add(team1);
+        getContentPane().add(team2);
+
+        team1entry.setBounds(750,10,500,100);
+        team1entry.setForeground(Color.WHITE);
+        getContentPane().add(team1entry);
+
+        team2entry.setBounds(750,120,500,100);
+        team2entry.setForeground(Color.WHITE);
+        getContentPane().add(team2entry);
+
+        calculate.setBounds(520,270,700,75);
+        calculate.setForeground(Color.blue);
+        calculate.setBackground(Color.black);
+        getContentPane().add(calculate);
+    }
+
+    public static void main(String[] args) {
+        Sports menu = new Sports();
+        menu.setTitle("Welcome to the Sports Simulation");
+        menu.setVisible(true);
+    }
 
 }
---
+++ src/Sports/SportsStats.java
@@ -0,0 +1,47 @@
+package Sports;
+// Made by Aryan Amberkar
+
+import javax.swing.*;
+import javax.swing.border.MatteBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SportsStats extends JFrame {
+
+    private JLabel BR;
+    private JLabel PS;
+    private JLabel CB;
+    private JLabel CBR;
+    private JLabel LC;
+    private JLabel OR;
+    private JLabel KC;
+    private JLabel DB;
+    private JLabel NE;
+    private JLabel NJ;
+    private JLabel BUF;
+    private JLabel MIA;
+    private JLabel IND;
+    private JLabel JAX;
+    private JLabel HOU;
+    private JLabel TEN;
+    private JLabel GB;
+    private JLabel CH;
+    private JLabel DE;
+    private JLabel MINN;
+    private JLabel NO;
+    private JLabel ATL;
+    private JLabel CAR;
+    private JLabel TB;
+    private JLabel SF;
+    private JLabel SEA;
+    private JLabel LAR;
+    private JLabel ARZ;
+    private JLabel DAL;
+    private JLabel PHI;
+    private JLabel NYG;
+    private JLabel WFT;
+
+
+
+}
