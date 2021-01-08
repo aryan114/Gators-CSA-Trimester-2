772a36ff12e90d98b7e31b728e7452c63f2ea43f
PNatarajan123 <dsanjay@gmail.com>
2021-01-08 08:21:41 -0800

    Update Itemspage.java

--- src/Eccomerce/Itemspage.java
+++ src/Eccomerce/Itemspage.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
 import java.util.List;
-
+//
 public class Itemspage extends JFrame {
     private JTextField itementry = new JTextField();
     private JButton search = new JButton();
